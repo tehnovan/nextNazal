29a40766e3c44175d4d91eb2177716897b4e6b9b
Georgy Shive <shive1997me@gmail.com>
2023-10-14 18:01:41 +0700

    Добавили кнопку отправки формы

--- src/entities/things/InputCard.tsx
+++ src/entities/things/InputCard.tsx
@@ -1,19 +1,39 @@
-
-import styles from './ui/Card.module.scss'
+import styles from "./ui/Card.module.scss";
 export default function InputCard() {
-    return (
-        <>
+const addProductDataBase = async (event: FormData) => {
+    const product = event.get("product")?.toString()
+    const price = event.get("price")?.toString()
 
-            <div>
-                <label className={styles.labelhead}>Заголовок</label>
-                <input type="text" className={styles.inputhead} />
-            </div>
-            <div className="mb-6">
-                <label className={styles.labelhead}>Устройство</label>
-                <input type="text" className={styles.inputdevice} />
-            </div>
+    if ( !product || !price ) return ;
+
+}
 
 
-        </>
-    )
+
+
+    
+  return (
+    <>
+      <form action={addProductDataBase} >
+        <div>
+          <label className={styles.labelhead}>Заголовок</label>
+          <input type="text" className={styles.inputhead} />
+        </div>
+        <div className="mb-6">
+          <label className={styles.labelhead}>Устройство</label>
+          <input type="text" className={styles.inputdevice} />
+        </div>
+        <div>
+          <button
+            type="submit"
+            className="relative inline-flex items-center justify-center p-0.5 mb-2 mr-2 overflow-hidden text-sm font-medium text-gray-900 rounded-lg group bg-gradient-to-br from-cyan-500 to-blue-500 group-hover:from-cyan-500 group-hover:to-blue-500 hover:text-white dark:text-white focus:ring-4 focus:outline-none focus:ring-cyan-200 dark:focus:ring-cyan-800"
+          >
+            <span className="relative px-5 py-2.5 transition-all ease-in duration-75 bg-white dark:bg-gray-900 rounded-md group-hover:bg-opacity-0">
+              Отправить
+            </span>
+          </button>
+        </div>
+      </form>
+    </>
+  );
 }
--- src/widgets/inputOutput/ui/InputOutput.tsx
+++ src/widgets/inputOutput/ui/InputOutput.tsx
@@ -1,16 +1,19 @@
-import InputCard from '@/entities/things/InputCard'
-import Card from '@/entities/things/ui/Card'
-import React from 'react'
+import InputCard from "@/entities/things/InputCard";
+import Card from "@/entities/things/ui/Card";
+import React from "react";
 
-export default async function InputOutput () {
-    const url = "https://65212453a4199548356cd8d4.mockapi.io/products"
-    const response = await fetch(url);
-    const products:products = await response.json()
+export default async function InputOutput() {
+  const url = "https://65212453a4199548356cd8d4.mockapi.io/products";
+  const response = await fetch(url);
+  const products: products[] = await response.json();
   return (
     <>
-    <InputCard/>
-     <Card head="VERSLA IDIOT" text= "МЕНЯ БАНЯТ ПО КД"/>
-     <Card head="нюхать беберу " text= "бебра"/>
-     </>
-  )
+      <InputCard />
+      {products.map((product) => (
+        <div key={product.id}>
+          <Card head={product.head} text={product.text} />
+        </div>
+      ))}
+    </>
+  );
 }
